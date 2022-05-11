458d6a5e7950602e4442f13c08c20ddd0939f431
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-05-11 11:40:08 +0300

    Added part6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
+import {CdkTableModule} from '@angular/cdk/table';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {MatTableModule} from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatSortModule } from '@angular/material/sort';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,30 +23,24 @@ import { UsersComponent } from './users-page/users/users.component';
 import { UserComponent } from './users-page/user/user.component';
 import { AddUserFormComponent } from './users-page/add-user-form/add-user-form.component';
 import { WeatherComponent } from './weather-page/weather/weather.component';
-import { HttpClientModule } from '@angular/common/http';
 import { UsersFilterPipe } from './users-page/users/users-filter.pipe';
 import { DynamicComponent } from './dynamic/dynamic.component';
 import { ClockComponent } from './clock/clock.component';
 import { StepperComponent } from './cdk-page/stepper/stepper.component';
-import { CdkStepperModule } from '@angular/cdk/stepper';
 import { CdkPageComponent } from './cdk-page/cdk-page/cdk-page.component';
 import { AccordionComponent } from './cdk-page/accordion/accordion.component';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
 import { TableComponent } from './cdk-page/table/table.component';
-import {CdkTableModule} from '@angular/cdk/table';
 import { DragAndDropComponent } from './cdk-page/drag-and-drop/drag-and-drop.component';
-import {DragDropModule} from '@angular/cdk/drag-drop';
 import { VirtualScrollComponent } from './cdk-page/virtual-scroll/virtual-scroll.component';
-import {ScrollingModule} from '@angular/cdk/scrolling';
 import { MatPageComponent } from './material-page/mat-page/mat-page.component';
 import { MatTableComponent } from './material-page/mat-table/mat-table.component';
-import {MatTableModule} from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatInputModule } from '@angular/material/input';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { MatStepperComponent } from './material-page/mat-stepper/mat-stepper.component';
+import { MatButtonModule } from '@angular/material/button';
+import { DatepickerComponent } from './material-page/datepicker/datepicker.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { SelectComponent } from './material-page/select/select.component';
+import { MatSelectModule } from '@angular/material/select';
+import { TooltipComponent } from './material-page/tooltip/tooltip.component';
 
 
 @NgModule({
@@ -51,7 +60,11 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     DragAndDropComponent,
     VirtualScrollComponent,
     MatPageComponent,
-    MatTableComponent
+    MatTableComponent,
+    MatStepperComponent,
+    DatepickerComponent,
+    SelectComponent,
+    TooltipComponent
   ],
   imports: [
     BrowserModule,
@@ -69,7 +82,13 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     MatPaginatorModule,
     MatNativeDateModule,
     MatInputModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSortModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
